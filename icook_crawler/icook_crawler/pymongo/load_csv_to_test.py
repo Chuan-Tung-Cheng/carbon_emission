a4af6ba04a23b930c300ca68050863e95c28387f
Chuan-Tung-Cheng <chengreentea0813@gmail.com>
2025-11-03 09:24:46 +0800

    add pymongo.py
    upload csv stored locally to mongodb set in the container of docker

--- icook_crawler/icook_crawler/pymongo/load_csv_to_test.py
+++ icook_crawler/icook_crawler/pymongo/load_csv_to_test.py
@@ -17,52 +17,75 @@ AUTH_DB=os.getenv("MONGO_AUTH_DB")
 
 BATCH_SIZE = 30 # upload in a batch of 30
 
-"""1. connect to MongoDB"""
-connection_string = f"mongodb://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/?authSource={AUTH_DB}"
-try:
-    client = mon.MongoClient(connection_string) # connecting
-    client.server_info()
-    print(client.server_info())
-    print("Authentication successful")
 
-except Exception as e:
-    print("Authentication failed")
-    raise e
+def connect_to_mongodb():
+    """1. connect to MongoDB"""
+    connection_string = f"mongodb://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/?authSource={AUTH_DB}"
+    try:
+        connection = mon.MongoClient(connection_string) # connecting
+        print(connection.server_info())
+        print("Authentication successful")
+        return connection
 
-db = client["mydatabase"] # connect to database
-collection = db["test"]
+    except Exception as e:
+        print(f"Authentication failed: {e}")
+        return e
 
-"""2. find csv path"""
-batch_to_insert = []
-file_amount = 0
-total_amount = 0
-csv_files = f"/Users/cct/Downloads/young/"
+def find_csv_file_path(root_dir):
+    """2. find csv path"""
+    csv_files = f"/Users/cct/Downloads/{root_dir}/"
+    return csv_files
 
-"""3. ready to upload csv files"""
-for csv_file_path in glob.glob(os.path.join(csv_files, "*.csv")):
-    file_amount += 1
-    with open(csv_file_path, mode="r", encoding="utf-8") as f:
-        # DictReader
-        reader = csv.DictReader(f)
-        for row in reader:
-            batch_to_insert.append(row)
+def upload_to_mongodb(connection, file_path):
+    """3. upload data to mongodb"""
 
-            # limit the amount of one-time uploading
-            if len(batch_to_insert) >= BATCH_SIZE:
-                collection.insert_many(batch_to_insert)
-                total_amount += len(batch_to_insert)
-                batch_to_insert.clear()
+    # connect to database
+    db = connection["mydatabase"]
+    # connect to selected table
+    collection = db["test"]
 
-"""4. upload data to mongodb"""
-if batch_to_insert:
-    collection.insert_many(batch_to_insert)
-    total_amount += len(batch_to_insert)
+    batch_to_insert = []
+    file_amount = 0
+    total_amount = 0
+
+    for csv_file_path in glob.glob(os.path.join(file_path, "*.csv")):
+        file_amount += 1
+        with open(csv_file_path, mode="r", encoding="utf-8") as f:
+            # DictReader
+            reader = csv.DictReader(f)
+            for row in reader:
+                batch_to_insert.append(row)
+
+                # limit the amount of one-time uploading
+                if len(batch_to_insert) >= BATCH_SIZE:
+                    collection.insert_many(batch_to_insert)
+                    total_amount += len(batch_to_insert)
+                    batch_to_insert.clear()
+
+    if batch_to_insert:
+        collection.insert_many(batch_to_insert)
+        total_amount += len(batch_to_insert)
+
+    print(f"""
+    {file_amount} files have been uploaded
+    {total_amount} data has been uploaded
+    """)
+
+def close_connection(db_connection):
+    """4. close mongodb"""
+    print("All data has been uploaded and mongodb is closed")
+    db_connection.close()
 
 
-"""5. close mongodb"""
-print(f"""
-{file_amount} files have been uploaded
-{total_amount} data has been uploaded
-""")
-client.close()
-print("All data has been uploaded and mongodb is closed")
+if __name__ == "__main__":
+    """1. connect to MongoDB"""
+    # get connected object, called connection
+    client = connect_to_mongodb()
+    """2. find csv path"""
+    # input files' root directory
+    pr_root_dir = input("root dir: ")
+    csv_path = find_csv_file_path(pr_root_dir)
+    """3. upload data to mongodb"""
+    upload_to_mongodb(client, csv_path)
+    """4. close mongodb"""
+    close_connection(client)
