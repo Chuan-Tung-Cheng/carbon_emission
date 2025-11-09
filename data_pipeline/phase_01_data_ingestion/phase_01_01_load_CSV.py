648a7c79078cc8e3946e658fcb73397299db3153
Chuan-Tung-Cheng <chengreentea0813@gmail.com>
2025-11-09 23:32:27 +0800

    Add archive function
    add archive function to prevent the processed dat from being processed again

--- data_pipeline/phase_01_data_ingestion/phase_01_01_load_CSV.py
+++ data_pipeline/phase_01_data_ingestion/phase_01_01_load_CSV.py
@@ -1,20 +1,37 @@
+import logging
 import os
+import sys
 from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
 CSV_DIR = os.getenv("CSV_DIR_2")
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 def find_csv_file_dir():
     """return generator of csv file path"""
     parent_dir = Path(CSV_DIR)
-    try:
-        csv_file_list = parent_dir.glob("*.csv")
-        return csv_file_list
-    except FileNotFoundError as e:
-        print(f"{parent_dir} does not have any csv files")
+    archive_dir = parent_dir / "archive"
+    if not parent_dir.exists():
+        print(f"Error: {parent_dir} does not have any csv files", file=sys.stderr)
+    archive_dir.mkdir(exist_ok=True, parents=True)  # parents=True 比較保險
+
+    csv_file_list = parent_dir.glob("*.csv")
+    for file in csv_file_list:
+        try:
+            yield file
+            if file.exists():
+                archive_path = archive_dir / file.name
+                file.replace(archive_path)
+                logger.info(f"[Generator] Archived finished file: {file.name}")
+
+        except FileNotFoundError as e:
+            print(f"{parent_dir} does not have any csv files")
+        except Exception as e:
+            logger.error(f"[Generator] Failed to archive {file.name}: {e}")
+
 
 if __name__ == "__main__":
-    csv = find_csv_file_dir()
-    for csv_file in csv:
-        print(csv_file)
+    find_csv_file_dir()
